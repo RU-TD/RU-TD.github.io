74f45837d02c855e012cba1adc884514cc76ce4f
tgrassi <tgrassi@nbi.dk>
2020-09-15 11:53:13 +0200

    conference2020 site upload

---
+++ conference2020/index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE HTML>
+<!--
+	Massively by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>RUTD Conference 2020</title>
+        <link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper" class="fade-in">
+
+				<!-- Intro -->
+					<div id="intro">
+						<h1>International conference<br />
+                        of the Research Unit<br />
+						"Planet Formation Witnesses and Probes: Transition Disks"</h1>
+						<p>October 7-9 2020 | Munich, Germany</p>
+						<ul class="actions">
+							<li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+						</ul>
+					</div>
+
+				<!-- Header -->
+					<header id="header">
+						<a href="index.php" class="logo">RUTD Conference 2020</a>
+					</header>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul class="links">
+							<li><a href="#objectives">Objectives</a></li>
+							<li><a href="#speakers">Speakers</a></li>
+							<li><a href="#programme">Programme</a></li>
+							<li><a href="#location">Location</a></li>
+						</ul>
+						<ul class="icons">
+							<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+						</ul>
+					</nav>
+
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- OBJECTIVES -->
+							<article class="post featured">
+                                <a name="objectives"></a>
+								<header class="major">
+									<h2>Objectives</h2>
+									<p>Transition disks, which are an advanced stage of circumstellar disks life showing evidence of an inner dust hole, are valuable witnesses of the planet formation process. DFG Research Unit FOR 2634/1 "Planet Formation Witnesses and Probes: Transition Disks" (<a href="https://www.transitiondiscs.com/" target="_blank">RUTD</a>) is focused on studying how the transition disks can be used to constrain protoplanetary disk evolution and planet formation. This conference will summarize the first funding period of the RUTD. During the conference, members of the RUTD will meet with invited international experts to exchange their scientific results and discuss areas of future development.</p>
+
+								</header>
+                                <!--<div style="text-align: center; display: block;">
+								<img src="images/pic01.jpg" alt="" style="width:100%;"/>
+                                </div>
+								<ul class="actions special">
+									<li><a href="#" class="button large">Full Story</a></li>
+								</ul>-->
+							</article>
+
+						<!-- SOC -->
+							<article class="post featured">
+                                <a name="objectives"></a>
+								<header class="major">
+									<h2>SOC</h2>
+						             <p>
+                                        <ul>
+                                            <li>Joanna Drążkowska (LMU Munich, chair)
+                                            <li>Anna Miotello (ESO, co-chair)
+                                            <li>Raphael Franz (LMU Munich)
+                                            <li>Tommaso Grassi (LMU Munich)
+                                            <li>Maria Koutoulaki (ESO)
+                                            <li>Kristina Monsch (LMU Munich)
+                                            <li>Giovanni Picogna (LMU Munich)
+                                            <li>Peter Rodenkirch (Heidelberg University)
+                                            <li>Thomas Rometsch (University of Tübingen)
+                                            <li>Apostolos Zormpas (LMU Munich)
+                                        </ul>
+                                    </p>
+
+								</header>
+                                <!--<div style="text-align: center; display: block;">
+								<img src="images/pic01.jpg" alt="" style="width:100%;"/>
+
+                                </div>
+
+								<ul class="actions special">
+
+									<li><a href="#" class="button large">Full Story</a></li>
+
+								</ul>-->
+							</article>
+
+
+
+						<!-- SPEAKERS -->
+							<article class="post featured" style="text-align:center;">
+                                <a name="speakers"></a>
+								<header class="major">
+									<h2>Invited Speakers</h2>
+                                <ul>
+                                    <li>Bertram Bitsch (MPIA Heidelberg)
+                                    <li>Richard Booth (University of Cambridge)
+                                    <li>Cathie Clarke (University of Cambridge)
+                                    <li>Uma Gorti (NASA Ames & SETI Institute)
+                                    <li>Akimasa Kataoka (National Astronomical Observatory of Japan)
+                                    <li>Anaelle Maury (CEA Saclay)
+                                    <li>Farzana Meru (University of Warwick)
+                                    <li>Riouhei Nakatani (RIKEN)
+                                    <li>Rebecca Nealon (University of Leicester)
+                                    <li>Geoffroy Lesur (Univ. Grenoble Alpes)
+                                    <li>James Owen (Imperial College London)
+                                    <li>Paola Pinilla (MPIA Heidelberg)
+                                    <li>Diana Powell (University of California, Santa Cruz)
+                                    <li>Giovanni Rosotti (Leiden University)
+                                    <li>Richard Teague (Harvard-Smithsonian Center for Astrophysics)
+                                </ul>
+
+								</header>
+
+								<!--<ul class="actions special">
+									<li><a href="#" class="button large">Full Story</a></li>
+								</ul>-->
+							</article>
+
+						<!-- PROGRAMME -->
+							<article class="post featured">
+                                <a name="programme"></a>
+								<header class="major">
+									<h2>Programme</h2>
+									<p>The conference starts on Wednesday, October 7th, at 9 am and ends on Friday, October 9th, at around 5 pm.<br>
+The detailed schedule will be announced later.
+</p>
+								<!--<ul class="actions special">
+									<li><a href="#" class="button large">Full Story</a></li>
+								</ul>-->
+							</article>
+
+
+						<!-- REGISTER
+							<article class="post featured">
+                                <a name="anchor_register"></a>
+								<header class="major">
+									<h2>Register</h2>
+								</header>
+                                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfOsSllbGE8cWPBlY0ZIIPQm8pVyGUH-WOsJ1aDRhCwySEG7w/viewform?embedded=true" width="640" height="1024" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+							</article>-->
+
+
+
+						<!-- Footer -->
+						<!--	<footer>
+								<div class="pagination">
+									<a href="#" class="page active">1</a>
+									<a href="#" class="page">2</a>
+									<a href="#" class="page">3</a>
+									<span class="extra">&hellip;</span>
+									<a href="#" class="page">8</a>
+									<a href="#" class="page">9</a>
+									<a href="#" class="page">10</a>
+									<a href="#" class="next">Next</a>
+								</div>
+							</footer>-->
+
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<section class="split contact">
+							<section style="padding-top: 20px; padding-bottom: 20px;">
+								<ul class="icons alt">
+									<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+								</ul>
+							</section>
+						</section>
+					</footer>
+
+				<!-- Copyright -->
+					<div id="copyright">
+						<ul><li>2020 &copy; RUTD</li><li>Design: TG and others, based on <a href="https://html5up.net/">HTML5 UP</a> | Image: NASA/JPL-Caltech</li></ul>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+	</body>
+</html>
